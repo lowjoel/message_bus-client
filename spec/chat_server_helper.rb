90fc35cffedaf95f517d4e58f941d89d6b6b6f9f
Michal Cichra <michal@cichra.cz>
2017-08-08 19:16:25 +0200

    [spec] run the chat server as part of rspec

--- spec/chat_server_helper.rb
+++ spec/chat_server_helper.rb
@@ -10,5 +10,27 @@ require 'message_bus'
 MessageBus.config[:backend] = :memory
 
 # Run the Chat Sinatra app.
-require "#{chat_example_path}/chat"
-run Chat
+require File.join(chat_example_path, 'chat')
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    Thread.abort_on_exception = true
+    Chat.set :port, 9292
+
+    Thread.new { Chat.run! }
+
+    retries = 0
+    begin
+      Socket.tcp('localhost', 9292, connect_timeout: 1)
+    rescue Errno::ECONNREFUSED
+      if (retries = retries.succ) < 10
+        sleep 1
+        retry
+      end
+    end
+  end
+
+  config.after(:suite) do
+    Chat.quit!
+  end
+end
